c789b6b04e7f9261c1c383e5f213a3530032ccb4
Kenan Karakecili <karakecili.kenan@gmail.com>
2016-07-11 14:04:18 +0300

    Update KKDateFormatter.swift

--- KKDateFormatter.swift
+++ KKDateFormatter.swift
@@ -8,22 +8,57 @@
 
 import Foundation
 
-class DateFormatter {
+class KKDateFormatter {
   
-  private static let current = DateFormatter()
+  enum DateCompare {
+    case Equal
+    case Left
+    case Right
+  }
+  
+  private static let current = KKDateFormatter()
   private let formatter = NSDateFormatter()
   private init() {}
+  
   class func convertDate(dateToFormat: AnyObject, fromFormat: String, toFormat: String) -> String {
-    DateFormatter.current.formatter.dateFormat = fromFormat
+    KKDateFormatter.current.formatter.dateFormat = fromFormat
     var date: NSDate? = nil
-    if object_getClassName(dateToFormat) == object_getClassName("") {
-      date = DateFormatter.current.formatter.dateFromString(dateToFormat as! String)
+    if dateToFormat is String {
+      date = KKDateFormatter.current.formatter.dateFromString(dateToFormat as! String)
     } else {
       date = dateToFormat as? NSDate
     }
-    DateFormatter.current.formatter.dateFormat = toFormat
-    let formattedDateString: String? = DateFormatter.current.formatter.stringFromDate(date!)
-    return formattedDateString != nil ? formattedDateString! : dateToFormat as! String
+    KKDateFormatter.current.formatter.dateFormat = toFormat
+    if let myDate = date {
+      let formattedDateString = KKDateFormatter.current.formatter.stringFromDate(myDate)
+      return formattedDateString
+    }
+    return ""
+  }
+  
+  class func compareDate(date1: AnyObject, date2: AnyObject, format: String) -> DateCompare {
+    KKDateFormatter.current.formatter.timeZone = NSTimeZone.localTimeZone()
+    KKDateFormatter.current.formatter.dateFormat = format
+    var dateOne: NSDate?
+    var dateTwo: NSDate?
+    if date1 is NSDate {
+      dateOne = date1 as? NSDate
+    } else {
+      dateOne = KKDateFormatter.current.formatter.dateFromString(date1 as! String)
+    }
+    if date2 is NSDate {
+      dateTwo = date2 as? NSDate
+    } else {
+      dateTwo = KKDateFormatter.current.formatter.dateFromString(date2 as! String)
+    }
+    let comparisonResult = dateOne!.compare(dateTwo!)
+    if comparisonResult == .OrderedSame {
+      return .Equal
+    } else if comparisonResult == .OrderedAscending {
+      return .Right
+    } else {
+      return .Left
+    }
   }
   
 }
